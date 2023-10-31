feecefdf486c92f33671de9467f77ce1587f7b15
Aaron Zaman <zaman.aaron2@gmail.com>
2023-10-30 17:55:41 -0700

    adding some seeder posts

--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -15,8 +15,27 @@ def seed_posts():
         Food for this week is sushi, start time, as usual, is 7pm.""",
         reply=True,
     )
+    general1 = Post(feedId=2, userId=1, type="none", title="Lost and Found",
+                     body="""This week we have found a jacket, watch, and one pair of house key. if you are missing any of these items, please, inquire with the synnagogue president or associated members. or treply to this post. """,
+                     reply=True,
+                )
+    general2 = Post(feedId=2, userId=1, type="donate", title="Raising funds for new chairs",
+                     body="""Our chairs have experienced some wear and tear. As such we are seeking to raise funds to purchase some new ones. Please, contribute if you can.""",
+                     reply=True,
+                )
+    general3 = Post(feedId=2, userId=1, type="none", title="Lost and Found",
+                     body="""The jacket from two weeks ago is still in the Lost and FOund. its is brown and very cozy... if anyone knows who's missing this jacket, please let us know.""",
+                     reply=True,
+                )
+    # general4 = Post(feedId=2, userId=1, type="none", title="filler",
+    #                  body="""filler""",
+    #                  reply=True,
+    #             )
     db.session.add(announcement)
     db.session.add(class1)
+    db.session.add(general1)
+    db.session.add(general2)
+    db.session.add(general3)
     db.session.commit()
 
 def undo_posts():
