8d9b465740c025593b637e3342d6ef85148448c8
Aaron Zaman <zaman.aaron2@gmail.com>
2024-03-19 04:11:19 -0700

    menu alterations soon

--- react-app/src/components/DropMenu/index.js
+++ react-app/src/components/DropMenu/index.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import './DropMenu.css'
 export default function DropMenu(props) {
-    
+    let {Passed} = props.Passed
     return(
         <div className='drop_menu'>
-
+            <Passed />
+            <div className='menu_button_tag'>
+                MENU
+            </div>
         </div>
     );
 }
